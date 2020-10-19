b3f660446233b2a00def9e762952ba5cc26f5c0d
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-19 21:52:12 +0300

    fix: fix script code

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,21 +1,16 @@
-const burgerButtonOpen = document.querySelector('.header-site-menu-burger-button--open');
+const burgerButton = document.querySelector('.header-site-menu-burger-button--open');
 const burgerButtonClose = document.querySelector('.header-site-menu-burger-button--close');
 const headerSiteMenu = document.querySelector('.header-site-menu');
-const headerLogo = document.querySelector('.header-logo');
 
 function openMobileMenu() {
   headerSiteMenu.classList.remove('header-site-menu--closed');
   headerSiteMenu.classList.add('header-site-menu--opened');
-  headerLogo.classList.add('visually-hidden');
-  burgerButtonOpen.classList.add('visually-hidden');
   headerSiteMenu.addEventListener('click', closeMobileMenuByClickOnOverlay);
 }
 
 function closeMobileMenu() {
   headerSiteMenu.classList.remove('header-site-menu--opened');
   headerSiteMenu.classList.add('header-site-menu--closed');
-  headerLogo.classList.remove('visually-hidden');
-  burgerButtonOpen.classList.remove('visually-hidden');
   headerSiteMenu.removeEventListener('click', closeMobileMenuByClickOnOverlay);
 }
 
@@ -25,14 +20,12 @@ function closeMobileMenuByClickOnOverlay(evt) {
   }
 }
 
-burgerButtonOpen.addEventListener('click', (evt) => {
+burgerButton.addEventListener('click', (evt) => {
   evt.preventDefault();
+  if (headerSiteMenu.classList.contains('header-site-menu--closed')) {
+    openMobileMenu();
+  } else if (headerSiteMenu.classList.contains('header-site-menu--opened')) {
+    closeMobileMenu();
+  }
 
-  openMobileMenu();
-});
-
-burgerButtonClose.addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  closeMobileMenu();
 });
