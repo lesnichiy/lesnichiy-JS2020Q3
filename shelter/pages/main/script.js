149dae27a9e34e17ac0e8a63224df0d692b182aa
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-20 22:36:19 +0300

    feat: add possibility close mobile menu by click on overlay

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,30 +1,23 @@
 const burgerButton = document.querySelector('.header-site-menu-burger-button');
 const headerInnerWrapper = document.querySelector('.header-inner-wrapper');
 const headerSiteMenuNav = document.querySelector('.header-site-menu');
+const overlayMobileMenu = document.querySelector('.header-overlay-mobile-menu');
 const body = document.querySelector('body');
 
-function openMobileMenu() {
-  headerWrapper.classList.remove('header--closed');
-  headerWrapper.classList.add('header--opened');
-  //header.addEventListener('click', closeMobileMenuByClickOnOverlay);
-}
-
-function closeMobileMenu() {
-  headerWrapper.classList.remove('header--opened');
-  headerWrapper.classList.add('header--closed');
-  //header.removeEventListener('click', closeMobileMenuByClickOnOverlay);
-}
-
-function closeMobileMenuByClickOnOverlay(evt) {
-  if (evt.target === header) {
-    closeMobileMenu();
-  }
-}
-
-burgerButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
+function toggleMobileMenu() {
   burgerButton.classList.toggle('header-site-menu-burger-button--opened');
   headerInnerWrapper.classList.toggle('header-inner-wrapper--opened');
   headerSiteMenuNav.classList.toggle('header-site-menu--opened');
   body.classList.toggle('scroll-hidden');
+  overlayMobileMenu.classList.toggle('header-overlay-mobile-menu--opened');
+}
+
+burgerButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  toggleMobileMenu();
+});
+
+overlayMobileMenu.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  toggleMobileMenu();
 });
