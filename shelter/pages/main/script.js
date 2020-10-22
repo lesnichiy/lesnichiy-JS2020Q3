0d3f963510efb6929e8dd4b7c14219733bf59891
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-22 16:40:19 +0300

    fix: add a declaration of body element

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -2,12 +2,13 @@ const burgerButton = document.querySelector('.header-site-menu-burger-button');
 const headerInnerWrapper = document.querySelector('.header-inner-wrapper');
 const headerSiteMenuNav = document.querySelector('.header-site-menu');
 const overlayMobileMenu = document.querySelector('.header-overlay-mobile-menu');
+const bodyElement = document.querySelector('body');
 
 function toggleMobileMenu() {
   burgerButton.classList.toggle('header-site-menu-burger-button--opened');
   headerInnerWrapper.classList.toggle('header-inner-wrapper--opened');
   headerSiteMenuNav.classList.toggle('header-site-menu--opened');
-  body.classList.toggle('scroll-hidden');
+  bodyElement.classList.toggle('scroll-hidden');
   overlayMobileMenu.classList.toggle('header-overlay-mobile-menu--opened');
 }
 
