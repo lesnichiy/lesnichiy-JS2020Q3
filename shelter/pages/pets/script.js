1417f12027ac554879a3f736bc198a040b3e9563
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-22 18:56:53 +0300

    feat: add html, css and js for mobile menu (pets page)

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,23 @@
+const burgerButton = document.querySelector('.header-site-menu-burger-button');
+const headerInnerWrapper = document.querySelector('.header-inner-wrapper');
+const headerSiteMenuNav = document.querySelector('.header-site-menu');
+const overlayMobileMenu = document.querySelector('.header-overlay-mobile-menu');
+const bodyElement = document.querySelector('body');
+
+function toggleMobileMenu() {
+  burgerButton.classList.toggle('header-site-menu-burger-button--opened');
+  headerInnerWrapper.classList.toggle('header-inner-wrapper--opened');
+  headerSiteMenuNav.classList.toggle('header-site-menu--opened');
+  bodyElement.classList.toggle('scroll-hidden');
+  overlayMobileMenu.classList.toggle('header-overlay-mobile-menu--opened');
+}
+
+burgerButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  toggleMobileMenu();
+});
+
+overlayMobileMenu.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  toggleMobileMenu();
+});
