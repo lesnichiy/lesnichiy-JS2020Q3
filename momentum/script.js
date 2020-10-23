f20604bfa7c987fc729964d5fd104ce35716c75c
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-24 01:09:09 +0300

    feat: add a days array and change names of the DOM elements in js

--- momentum/script.js
+++ momentum/script.js
@@ -1,11 +1,13 @@
 const COLOR_PRIMARY = '#000000';
 const COLOR_PRIMARY_CONTRAST = '#ffffff';
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const time = document.querySelector('#time');
-const greeting = document.querySelector('#greeting');
-const name = document.querySelector('#name');
-const focus = document.querySelector('#focus');
-const overlay = document.querySelector('.overlay');
+const timeElem = document.querySelector('#time');
+const dayElem = document.querySelector('#day')
+const greetingElem = document.querySelector('#greeting');
+const nameElem = document.querySelector('#name');
+const focusElem = document.querySelector('#focus');
+const overlayElem = document.querySelector('.overlay');
 
 //Show Time
 function showTime() {
@@ -13,8 +15,10 @@ function showTime() {
   let hours = today.getHours();
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
+  let day = today.getDay();
 
-  time.innerHTML = `${addZero(hours)}<span>:</span>${addZero(minutes)}<span>:</span>${addZero(seconds)}`;
+  timeElem.innerHTML = `${addZero(hours)}<span>:</span>${addZero(minutes)}<span>:</span>${addZero(seconds)}`;
+
 
   setTimeout(showTime, 1000)
 }
@@ -32,36 +36,36 @@ function setBgAndGreeting() {
   if (hour < 6) {
     //Night
     document.body.style.backgroundImage = `url(assets/images/night/02.jpg)`;
-    greeting.textContent = 'Good Night';
+    greetingElem.textContent = 'Good Night';
     document.body.style.color = COLOR_PRIMARY_CONTRAST;
-    overlay.style.backgroundColor = COLOR_PRIMARY;
+    overlayElem.style.backgroundColor = COLOR_PRIMARY;
   } else if (hour < 12) {
     //Morning
     document.body.style.backgroundImage = `url(assets/images/morning/11.jpg)`;
-    greeting.textContent = 'Good Morning';
+    greetingElem.textContent = 'Good Morning';
     document.body.style.color = COLOR_PRIMARY;
-    overlay.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
+    overlayElem.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
   } else if (hour < 18) {
     //Afternoon
     document.body.style.backgroundImage = `url(assets/images/day/11.jpg)`;
-    greeting.textContent = 'Good Afternoon';
+    greetingElem.textContent = 'Good Afternoon';
     document.body.style.color = COLOR_PRIMARY;
-    overlay.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
+    overlayElem.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
   } else {
     //Evening
     document.body.style.backgroundImage = `url(assets/images/evening/10.jpg)`;
-    greeting.textContent = 'Good Evening';
+    greetingElem.textContent = 'Good Evening';
     document.body.style.color = COLOR_PRIMARY_CONTRAST;
-    overlay.style.backgroundColor = COLOR_PRIMARY;
+    overlayElem.style.backgroundColor = COLOR_PRIMARY;
   }
 }
 
 //Get Name from localStorage
 function getName() {
   if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Your Name Here]';
+    nameElem.textContent = '[Enter Your Name Here]';
   } else {
-    name.textContent = localStorage.getItem('name');
+    nameElem.textContent = localStorage.getItem('name');
   }
 }
 
@@ -70,7 +74,7 @@ function setName(evt) {
   if (evt.type === 'keypress') {
     if (evt.which === 13 || evt.keyCode === 13) {
       localStorage.setItem('name', evt.target.innerText);
-      name.blur();
+      nameElem.blur();
     }
   } else {
     localStorage.setItem('name', evt.target.innerText);
@@ -80,9 +84,9 @@ function setName(evt) {
 //Get Focus from localStorage
 function getFocus() {
   if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Your Focus For Today Here]';
+    focusElem.textContent = '[Enter Your Focus For Today Here]';
   } else {
-    focus.textContent = localStorage.getItem('focus');
+    focusElem.textContent = localStorage.getItem('focus');
   }
 }
 
@@ -91,17 +95,17 @@ function setFocus(evt) {
   if (evt.type === 'keypress') {
     if (evt.which === 13 || evt.keyCode === 13) {
       localStorage.setItem('focus', evt.target.innerText);
-      focus.blur();
+      focusElem.blur();
     }
   } else {
     localStorage.setItem('focus', evt.target.innerText);
   }
 }
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
+nameElem.addEventListener('keypress', setName);
+nameElem.addEventListener('blur', setName);
+focusElem.addEventListener('keypress', setFocus);
+focusElem.addEventListener('blur', setFocus);
 
 
 
