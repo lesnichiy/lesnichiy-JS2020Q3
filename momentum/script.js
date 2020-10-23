85db0dcd129d343f4592f2a7cf50a5d0ac7b13e0
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-23 20:47:29 +0300

    feat: declarate variables of DOM elements in js file

---
+++ momentum/script.js
@@ -0,0 +1,4 @@
+const time = document.querySelector('#time');
+const greeting = document.querySelector('#greeting');
+const name = document.querySelector('#name');
+const focus = document.querySelector('#focus');
