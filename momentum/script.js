864c22ecd92b6ffd36cb0c73dd4afc18aed7cba2
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-23 21:27:21 +0300

    feat: add a functions showTime, addZero and setBgAndGreeting in js file

--- momentum/script.js
+++ momentum/script.js
@@ -2,3 +2,53 @@ const time = document.querySelector('#time');
 const greeting = document.querySelector('#greeting');
 const name = document.querySelector('#name');
 const focus = document.querySelector('#focus');
+
+//Show Time
+function showTime() {
+  let today = new Date();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+
+  const amPm = hour >= 12 ? 'PM' : 'AM';
+
+  //12hr Format
+  hour = hour % 12 || 12;
+
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+  setTimeout(showTime, 1000)
+}
+
+//add Zeros
+function addZero(n) {
+  return ((parseInt(n, 10) < 10) ? '0' : '') + n;
+}
+
+//Set Background and Greeting
+function setBgAndGreeting() {
+  let today = new Date();
+  let hour = today.getHours();
+
+  if (hour < 6) {
+    //Night
+    document.body.style.backgroundImage = `url(assets/images/night/01.jpg)`;
+    greeting.textContent = 'Good Night';
+  } else if (hour < 12) {
+    //Morning
+    document.body.style.backgroundImage = `url(assets/images/morning/01.jpg)`;
+    greeting.textContent = 'Good Morning';
+  } else if (hour < 18) {
+    //Afternoon
+    document.body.style.backgroundImage = `url(assets/images/afternoon/01.jpg)`;
+    greeting.textContent = 'Good Afternoon';
+  } else {
+    //Evening
+    document.body.style.backgroundImage = `url(assets/images/evening/01.jpg)`;
+    greeting.textContent = 'Good Evening';
+  }
+}
+
+
+showTime();
+setBgAndGreeting();
