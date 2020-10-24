ccdea0d28aa7747e9fb76ed3b3eba6e9bdb1d143
Vladimir Popkov <lesnichiy02@ya.ru>
2020-10-25 01:17:15 +0300

    feat: make a background images functionality (generate arr of bg img, change bg every hour, change by click on button, smooth change bg images)

--- momentum/script.js
+++ momentum/script.js
@@ -2,9 +2,10 @@ const COLOR_PRIMARY = '#000000';
 const COLOR_PRIMARY_CONTRAST = '#ffffff';
 const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const IMAGES_URLs_PER_HOUR = createBgImgDayArrURLs();
 
 const timeElem = document.querySelector('#time');
-const dayElem = document.querySelector('#day')
+const dayElem = document.querySelector('#day');
 const greetingElem = document.querySelector('#greeting');
 const nameElem = document.querySelector('#name');
 const focusElem = document.querySelector('#focus');
@@ -23,6 +24,10 @@ function showTime() {
   timeElem.innerHTML = `${addZero(hours)}<span>:</span>${addZero(minutes)}<span>:</span>${addZero(seconds)}`;
   dayElem.innerHTML = `${DAYS[day]}, ${date} ${MONTHS[month]}`;
 
+  if ( minutes === 0 && seconds === 0) {
+    setBgAndGreeting();
+  }
+
 
   setTimeout(showTime, 1000)
 }
@@ -32,32 +37,88 @@ function addZero(n) {
   return ((parseInt(n, 10) < 10) ? '0' : '') + n;
 }
 
+//get random number in range
+function randomNumber(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+//create array with url of 24 random images
+function createBgImgDayArrURLs() {
+  const BASE_BG_IMG_URL = `assets/images/`;
+  const MIN_IMG_NUM = 1;
+  const MAX_IMG_NUM = 20;
+  const DAY_HOURS = 24;
+
+  const resultArr = [];
+
+  for (let i = 0; i < DAY_HOURS; i++) {
+    let imgRandName = `${addZero(randomNumber(MIN_IMG_NUM, MAX_IMG_NUM))}.jpg`;
+
+    if (i < 6) {
+      //Night
+      resultArr.push(`${BASE_BG_IMG_URL}night/${imgRandName}`);
+    } else if (i < 12) {
+      //Morning
+      resultArr.push(`${BASE_BG_IMG_URL}morning/${imgRandName}`);
+    } else if (i < 18) {
+      //day
+      resultArr.push(`${BASE_BG_IMG_URL}day/${imgRandName}`);
+    } else {
+      //Evening
+      resultArr.push(`${BASE_BG_IMG_URL}evening/${imgRandName}`);
+    }
+  }
+
+  return resultArr;
+}
+
+//smooth change img
+function viewBgImage(url) {
+  const body = document.querySelector('body');
+  const img = document.createElement('img');
+  img.src = url;
+  img.onload = () => {
+    body.style.backgroundImage = `url(${url})`;
+  };
+}
+
+//change bg image by button
+const changeBgButton = document.querySelector('.change-bg-img-button');
+const currentDate = new Date();
+let currentHour = currentDate.getHours();
+function changeBgImage() {
+  currentHour++;
+  currentHour = currentHour % 24;
+  viewBgImage(IMAGES_URLs_PER_HOUR[currentHour]);
+  changeBgButton.disabled = true;
+  setTimeout(function() { changeBgButton.disabled = false }, 1000);
+}
+changeBgButton.addEventListener('click', changeBgImage);
+
 //Set Background and Greeting
 function setBgAndGreeting() {
   let today = new Date();
   let hour = today.getHours();
 
+  viewBgImage(IMAGES_URLs_PER_HOUR[hour]);
+
   if (hour < 6) {
     //Night
-    document.body.style.backgroundImage = `url(assets/images/night/02.jpg)`;
     greetingElem.textContent = 'Good Night, ';
     document.body.style.color = COLOR_PRIMARY_CONTRAST;
     overlayElem.style.backgroundColor = COLOR_PRIMARY;
   } else if (hour < 12) {
     //Morning
-    document.body.style.backgroundImage = `url(assets/images/morning/11.jpg)`;
     greetingElem.textContent = 'Good Morning, ';
     document.body.style.color = COLOR_PRIMARY;
     overlayElem.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
   } else if (hour < 18) {
     //Afternoon
-    document.body.style.backgroundImage = `url(assets/images/day/11.jpg)`;
     greetingElem.textContent = 'Good Afternoon, ';
     document.body.style.color = COLOR_PRIMARY;
     overlayElem.style.backgroundColor = COLOR_PRIMARY_CONTRAST;
   } else {
     //Evening
-    document.body.style.backgroundImage = `url(assets/images/evening/10.jpg)`;
     greetingElem.textContent = 'Good Evening, ';
     document.body.style.color = COLOR_PRIMARY_CONTRAST;
     overlayElem.style.backgroundColor = COLOR_PRIMARY;
@@ -124,6 +185,8 @@ function setFocus(evt) {
   }
 }
 
+
+//Event Listeners of Name and Focus
 nameElem.addEventListener('click', (evt) => evt.target.textContent = '');
 nameElem.addEventListener('keypress', setName);
 nameElem.addEventListener('blur', setName);
@@ -133,6 +196,7 @@ focusElem.addEventListener('blur', setFocus);
 
 
 
+//Run
 showTime();
 setBgAndGreeting();
 getName();
