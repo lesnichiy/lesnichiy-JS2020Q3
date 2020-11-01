4096fbd5e6cf36fca6c5eba31ad7a738ce41badf
Vladimir Popkov <lesnichiy02@ya.ru>
2020-11-01 18:31:11 +0300

    feat: add a backlight for buttons by click on real keyboard, add switch language method

--- keyboard/js/Keyboard.js
+++ keyboard/js/Keyboard.js
@@ -45,5 +45,58 @@ export default class Keyboard {
         }
       });
     });
+
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
   }
+
+  handleEvent = (evt) => {
+    if (evt.stopPropagation) evt.stopPropagation();
+    const {code, type} = evt;
+    const keyObj = this.keyButtons.find((key) => key.code === code);
+    if (!keyObj) return;
+    this.output.focus();
+
+    if (type.match(/keydown|mousedown/)) {
+      if (type.match(/key/)) evt.preventDefault();
+      keyObj.div.classList.add('active');
+
+      //Switch language
+      if (code.match(/Control/)) this.ctrlKey = true;
+      if (code.match(/Alt/)) this.altKey = true;
+
+      if (code.match(/Control/) && this.altKey) this.switchLanguage();
+      if (code.match(/Alt/) && this.ctrlKey) this.switchLanguage();
+
+    } else if (type.match(/keyup|mouseup/)) {
+      keyObj.div.classList.remove('active');
+
+      //Switch language
+      if (code.match(/Control/)) this.ctrlKey = false;
+      if (code.match(/Alt/)) this.altKey = false;
+    }
+  };
+
+  switchLanguage = () => {
+    const langAbbr = Object.keys(language);
+    let langInx = langAbbr.indexOf(this.container.dataset.language);
+    this.keyBase = langInx + 1  < langAbbr.length ? language[langAbbr[langInx += 1]]
+        : language[langAbbr[langInx -= langInx]];
+
+    this.container.dataset.language = langAbbr[langInx];
+    storage.set('kbLang', langAbbr[langInx]);
+
+    this.keyButtons.forEach( (button) => {
+      const keyObj = this.keyBase.find((key) => key.code === button.code);
+      if (!keyObj) return;
+      button.shift = keyObj.shift;
+      button.small = keyObj.small;
+      if (keyObj.shift && keyObj.shift.match(/^a-zA-Zа-яА-ЯёЁ0-9/g)) {
+        button.sub.innerHTML = keyObj.shift;
+      } else {
+        button.sub.innerHTML = '';
+      }
+      button.letter.innerHTML = keyObj.small;
+    });
+  };
 }
